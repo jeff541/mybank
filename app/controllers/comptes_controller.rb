a0e6ab83c96858b8052cb36ec3a2b0d60c5b85f9
Jeff Mbiada <jeff@itasapro.com>
2024-04-19 00:25:00 +0100

    edit 2

--- app/controllers/comptes_controller.rb
+++ app/controllers/comptes_controller.rb
@@ -1,9 +1,13 @@
 class ComptesController < ApplicationController
-  before_action :set_compte, only: %i[ show edit update destroy ]
+  before_action :set_compte, only: %i[ show edit update destroy set_amount update_amount ]
 
   # GET /comptes or /comptes.json
   def index
     @comptes = Compte.all
+    if params[:search].present?
+      @comptes = @comptes.where("LOWER(name) ILIKE :search ",
+                                      search: "%#{params[:search][:criteria].downcase}%")
+    end
   end
 
   # GET /comptes/1 or /comptes/1.json
@@ -18,7 +22,16 @@ class ComptesController < ApplicationController
   # GET /comptes/1/edit
   def edit
   end
+  def set_amount
 
+  end
+  def update_amount
+    @compte = Compte.find(params[:id])
+    transaction_type = params[:compte][:transaction_type]
+    transaction_amount = params[:compte][:transaction_amount].to_i
+    @compte.set_amount(transaction_amount,transaction_type)
+      redirect_to comptes_path, notice: 'Amount updated successfully.'
+  end
   # POST /comptes or /comptes.json
   def create
     @compte = Compte.new(compte_params)
@@ -38,7 +51,7 @@ class ComptesController < ApplicationController
   def update
     respond_to do |format|
       if @compte.update(compte_params)
-        format.html { redirect_to compte_url(@compte), notice: "Compte was successfully updated." }
+        format.html { redirect_to comptes_path, notice: "Compte was successfully updated." }
         format.json { render :show, status: :ok, location: @compte }
       else
         format.html { render :edit, status: :unprocessable_entity }
